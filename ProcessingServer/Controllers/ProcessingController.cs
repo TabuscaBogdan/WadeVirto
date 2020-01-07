59bb77af0c977588bb0eccc9c14125b46c89102b
Bogdan Tabusca <btabusca@endava.com>
2020-01-07 17:07:02 +0200

    Added useful comments & stuff

--- ProcessingServer/Controllers/ProcessingController.cs
+++ ProcessingServer/Controllers/ProcessingController.cs
@@ -14,6 +14,26 @@ namespace ProcessingServer.Controllers
     public class ProcessingController : ControllerBase
     {
         [HttpPost("[action]")]
+
+
+        //TODO The KEY!!!
+        /*
+         * SELECT DISTINCT ?item
+           WHERE
+           {
+           ?item rdfs:label "Hammerfall"
+           } to get artist id
+
+            SELECT DISTINCT ?property ?hasValue ?isValueOf
+           WHERE {
+           { <http://dbtune.org/musicbrainz/resource/artist/00039b8a-3da6-4cb2-85e3-f93e30f43049> ?property ?hasValue 
+           }
+           }
+           ORDER BY (!BOUND(?hasValue)) ?property ?hasValue ?isValueOf
+           to get information about artist, use /artist id you got from previous querry
+         */
+
+
         public List<JsonLDSong> SeekSongs([FromBody] string request)
         {
             var songs = new List<JsonLDSong>();
