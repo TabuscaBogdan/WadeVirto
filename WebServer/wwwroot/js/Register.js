4e53021f246f926535174ed49349fd7614daeed6
Bogdan <bogdan.tabusca@gmail.com>
2020-01-30 09:34:11 +0200

    Added the Scholarity page Fixed the Register Page.

--- WebServer/wwwroot/js/Register.js
+++ WebServer/wwwroot/js/Register.js
@@ -14,6 +14,8 @@
 async function SendRegistration() {
     var form = document.forms["registerForm"].getElementsByTagName("input");
     var jbody = {}
+    var email = form[0].value;
+
     jbody["Email"] = form[0].value;
     jbody["Password"] = form[1].value;
     jbody["Re-Password"] = form[2].value;
@@ -21,6 +23,13 @@ async function SendRegistration() {
     if (jbody["Password"] === jbody["Re-Password"]) {
         jbody = JSON.stringify(jbody);
         var response = await RegisterUser(jbody);
+        if (response["token"] !== "Login failed!") {
+            localStorage.setItem("MusicEmail", email);
+            localStorage.setItem("VirtoUserToken", response["token"]);
+            window.location.replace('https://localhost:44372/MainPage.html');
+        } else {
+            window.alert("Login Failed!");
+        }
     } else {
         window.alert("Passwords do not match!");
     }
